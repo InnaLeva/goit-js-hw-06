245302f0afea5acd1288e61decc404618637b7c1
Inna Ratushna <108590500+InnaLeva@users.noreply.github.com>
2023-02-14 22:17:07 +0200

    add change

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,8 @@
 const rangeInput = document.querySelector('#font-size-control');
 const spanText = document.querySelector('span#text');
 
-rangeInput.addEventListener('change', onInputRangeChange);
+rangeInput.addEventListener('input', onInputRangeChange);
 
 function onInputRangeChange(event) {
-    spanText.getElementsByClassName.fontSize = `${event.currentTarget.value}px`;
+    spanText.style.fontSize = `${event.currentTarget.value}px`;
 }
